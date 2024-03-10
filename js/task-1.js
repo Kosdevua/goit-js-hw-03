4e6c8dfbf6f4f485c71141676d20e4a4f11f2e2b
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-03-10 22:07:29 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,12 @@
-console.log('hello');
+// Задача 1. Генератор slug
+
+function slugify(title) {
+  let slugArray = title.toLowerCase().split(' ');
+  let slugiString = slugArray.join('-');
+  return slugiString;
+}
+
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
